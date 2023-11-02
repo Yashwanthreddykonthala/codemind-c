5e727426856019d1d374888d93e7c695ce7ab49d
Yashwanthreddykonthala <147489808+Yashwanthreddykonthala@users.noreply.github.com>
2023-11-02 23:02:56 +0530

    #include<stdio.h> int main() {     float x,y,loss_percentage;     scanf("%f%f",&x,&y);     loss_percentage=((x-y)/x)*100;     printf("%.2f",loss_percentage);     return 0;
    }

---
+++ Loss_Percentage.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float x,y,loss_percentage;
+    scanf("%f%f",&x,&y);
+    loss_percentage=((x-y)/x)*100;
+    printf("%.2f",loss_percentage);
+    return 0;
+    
+}
