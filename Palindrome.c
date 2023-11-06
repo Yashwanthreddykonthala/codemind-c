14a5771a411bff3a70920261a7b5cb2f3100047c
Yashwanthreddykonthala <147489808+Yashwanthreddykonthala@users.noreply.github.com>
2023-11-06 15:15:52 +0530

    ///palindrome=if a  number is reverse of a given number then that is called palindrome /// #include<stdio.h> int main() { 	int n,i,r,rev=0,t; 	scanf("%d",&n); 	t=n; 	while(n!=0) 	{ 		r=n%10; 		rev=rev*10+r; 		n=n/10; 	} 	if(t==rev) 	{ 		printf("Palindrome"); 	} 	else 	{ 		printf("Not Palindrome"); 	} }

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+///palindrome=if a  number is reverse of a given number then that is called palindrome ///
+#include<stdio.h>
+int main()
+{
+	int n,i,r,rev=0,t;
+	scanf("%d",&n);
+	t=n;
+	while(n!=0)
+	{
+		r=n%10;
+		rev=rev*10+r;
+		n=n/10;
+	}
+	if(t==rev)
+	{
+		printf("Palindrome");
+	}
+	else
+	{
+		printf("Not Palindrome");
+	}
+}
