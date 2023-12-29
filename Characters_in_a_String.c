eb0119062cb2d72e78536263b7f2ef8ab12e67cb
Yashwanthreddykonthala <147489808+Yashwanthreddykonthala@users.noreply.github.com>
2023-12-29 22:38:41 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[20];     s[20];     int i=0,c=0;     scanf("%[^ ]s",&s);     for(i=0;s[i]!=NULL;i++) {         c++;     }     printf("%d",c); }

---
+++ Characters_in_a_String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char s[20];
+    s[20];
+    int i=0,c=0;
+    scanf("%[^
+]s",&s);
+    for(i=0;s[i]!=NULL;i++) {
+        c++;
+    }
+    printf("%d",c);
+}
